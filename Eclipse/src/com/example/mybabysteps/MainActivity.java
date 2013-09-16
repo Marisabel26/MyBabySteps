9bb0a087dc741f9091a4510ee73cb5902ddb9741
Marisabel <marisabelb@gmail.com>
2013-09-15 23:20:24 -0600

    Actualizando avance de la aplicación

--- Eclipse/src/com/example/mybabysteps/MainActivity.java
+++ Eclipse/src/com/example/mybabysteps/MainActivity.java
@@ -2,14 +2,28 @@ package com.example.mybabysteps;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
+	
+	Button btnCrear;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        /*btnCrear = (Button)findViewById(R.id.btnCrear);
+        btnCrear.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				startActivity(new Intent(MainActivity.this,Categorias.class));
+			}
+		});*/
     }
 
     @Override
@@ -17,4 +31,11 @@ public class MainActivity extends Activity {
         getMenuInflater().inflate(R.menu.activity_main, menu);
         return true;
     }
+ 
+    
+	public void irAcrear(View view) {
+		Intent intento = new Intent(this, Categorias.class);
+		startActivity(intento);
+		finish();
+	}
 }
