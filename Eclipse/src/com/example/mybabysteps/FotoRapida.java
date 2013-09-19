81bc6fbbc15018b0d35c2be1e6ccba88fe1d2e70
Marisabel <marisabelb@gmail.com>
2013-09-18 22:03:27 -0600

    Se agregan cambios de Paola

--- Eclipse/src/com/example/mybabysteps/FotoRapida.java
+++ Eclipse/src/com/example/mybabysteps/FotoRapida.java
@@ -1,20 +1,37 @@
 package com.example.mybabysteps;
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.support.v4.app.NavUtils;
+import android.hardware.Camera.*;
+
 
 public class FotoRapida extends Activity {
-
+	private static final int CAPTURE_VIDEO_ACTIVITY_REQUEST_CODE = 200;
+	public static final int MEDIA_TYPE_IMAGE = 1;
+	private Uri fileUri;
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_foto_rapida);
-        getActionBar().setDisplayHomeAsUpEnabled(true);
-    }
+        //getActionBar().setDisplayHomeAsUpEnabled(true);
+        
+        // create Intent to take a picture and return control to the calling application
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
 
+       // fileUri = getOutputMediaFileUri(MEDIA_TYPE_IMAGE); // create a file to save the image
+     //   intent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri); // set the image file name
+
+        // start the image capture Intent
+        startActivityForResult(intent, 200);   
+    }
+    
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.activity_foto_rapida, menu);
@@ -31,5 +48,5 @@ public class FotoRapida extends Activity {
         }
         return super.onOptionsItemSelected(item);
     }
-
 }
+
